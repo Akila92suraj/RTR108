51afb5707a5a20ae0faa5f1de44b653dca53a3d4
user <sachiakilasuraj@gmail.com>
2020-02-13 10:15:09 +0200

    20200213_10_15

---
+++ diary_20200213.py
@@ -0,0 +1,172 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> type('Hello,World!')
+<class 'str'>
+>>> type(17)
+<class 'int'>
+>>> type(3.2)
+<class 'float'>
+>>> type('17')
+<class 'str'>
+>>> type(3.2)
+<class 'float'>
+>>> print(1000000)
+1000000
+>>> print(1,000,000)
+1 0 0
+>>> massage = 'Ānd now for something completety different'
+>>> n = 17
+>>> pi = 3.1415926535897931
+>>> print(n)
+17
+>>> print(pi)
+3.141592653589793
+>>> type(massage)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+\
+>>> type(pi)
+<class 'float'>
+>>> print(n)
+17
+>>> 76trombones = 'big parade'
+SyntaxError: invalid syntax
+>>> more@ = 1000000
+SyntaxError: invalid syntax
+>>> class ='Ādvanced Theoritical Zymurgy'
+SyntaxError: invalid syntax
+>>> 
+and
+SyntaxError: invalid syntax
+>>> as
+SyntaxError: invalid syntax
+>>> awit
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    awit
+NameError: name 'awit' is not defined
+>>> print(1)
+1
+>>> x = 2
+>>> print(x)
+2
+>>> x = 1;2
+2
+>>> x = [ 1; 2]
+SyntaxError: invalid syntax
+>>> x = [1 , 2]
+>>> print (x)
+[1, 2]
+>>> x = {1 , 2}
+>>> print(x)
+{1, 2}
+>>> 20+30
+50
+>>> hour-
+SyntaxError: invalid syntax
+>>> hour*60+minute
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    hour*60+minute
+NameError: name 'hour' is not defined
+>>> hour = 24
+>>> hour-1
+23
+>>> hour = 1
+>>> minute = hour*60
+>>> hour*60+minute
+120
+>>> minute/60
+1.0
+>>> 5**2
+25
+>>> (5+9)*(15-7)
+112
+>>> minute= 59
+>>> minute/60
+0.9833333333333333
+>>> minute//60
+0
+>>> 17
+17
+>>> x
+{1, 2}
+>>> x+17
+Traceback (most recent call last):
+  File "<pyshell#48>", line 1, in <module>
+    x+17
+TypeError: unsupported operand type(s) for +: 'set' and 'int'
+>>> 1+1
+2
+>>> x= 5
+>>> x + 1
+6
+>>> 2*(3-1)
+4
+>>> (1+1)**(5-1)
+16
+>>> (minute*100) / 60
+98.33333333333333
+>>> 2**1+1
+3
+>>> 3*1**3
+3
+>>> 3**3*1
+27
+>>> 3*1
+3
+>>> 3*1**3
+3
+>>> 3*1***3
+SyntaxError: invalid syntax
+>>> 3*1**3*3
+9
+>>> 2*3-1
+5
+>>> 6+4/2
+8.0
+>>> 5-3-1
+1
+>>> 5-3
+2
+>>> quotient =7//3
+>>> print(quotient)
+2
+>>> remainder = 7% 3
+>>> print(remainder)
+1
+>>> frist = 10
+>>> second = 15
+>>> print(frist+second)
+25
+>>> frist = 100
+>>> second = '150'
+>>> print(frist + second)
+Traceback (most recent call last):
+  File "<pyshell#75>", line 1, in <module>
+    print(frist + second)
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> frist ='100'
+>>> second ='150'
+>>> print(frist+second)
+100150
+>>> frist = 'Test'
+>>> second = 3
+>>> print(frist* second)
+TestTestTest
+>>> int = input()
+Some silly stuff
+>>> print(int)
+Some silly stuff
+>>> name =
+SyntaxError: invalid syntax
+>>> name = input('What is your name?\n')
+What is your name?
+chuck
+>>> print(name)
+chuck
+>>> 
